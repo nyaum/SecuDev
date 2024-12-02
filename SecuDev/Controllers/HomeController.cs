90c18adbe0eda67561f415f95022ae21ad548b54
문현준 <mhj45072@secuidea.com>
2024-12-02 17:35:59 +0900

    초기 커밋.

---
+++ SecuDev/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using FrameWork.DB;
+using SecuDev.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAdmin.Models;
+
+namespace SecuDev.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            SqlParamCollection param = new SqlParamCollection();
+
+            DataSet ds = (new Common()).MdlList(param, "usp_GetLatestUpdatesByLocation");
+
+            List<Location> list = new List<Location>();
+            
+            for(int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+
+                Location l = new Location();
+
+                l.LocationName = ds.Tables[0].Rows[i]["LocationName"].ToString();
+                l.CorpsName = ds.Tables[0].Rows[i]["CorpsName"].ToString();
+                l.GateName = ds.Tables[0].Rows[i]["GateName"].ToString();
+                l.InstallationDate = ds.Tables[0].Rows[i]["InstallationDate"].ToString();
+                l.InstallationType = ds.Tables[0].Rows[i]["InstallationType"].ToString();
+                l.SoftwareName = ds.Tables[0].Rows[i]["SoftwareName"].ToString();
+                l.Version = ds.Tables[0].Rows[i]["Version"].ToString();
+                l.Notes = ds.Tables[0].Rows[i]["Notes"].ToString();
+
+                list.Add(l);
+            }
+
+            ViewBag.list = list;
+
+            return View();
+        }
+
+        public ActionResult Setup()
+        {
+
+
+            return View();
+        }
+
+    }
+}
---
+++ SecuDev/Models/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SecuDev.Models
+{
+    public class Location
+    {
+        public string LocationName { get; set; }
+        public string CorpsName { get; set; }
+        public string GateName { get; set; }
+        public string InstallationDate { get; set; }
+        public string InstallationType { get; set; }
+        public string SoftwareName { get; set; }
+        public string Version { get; set; }
+        public string Notes { get; set; }
+    }
+}
