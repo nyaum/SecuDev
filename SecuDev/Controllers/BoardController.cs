3cc0d8e707e7a6c3fa41482547a8fd5ab3081523
문현준 <mhj45072@secuidea.com>
2024-12-10 17:00:11 +0900

    게시판 디자인 작업

---
+++ SecuDev/Controllers/BoardController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecuDev.Controllers
+{
+    public class BoardController : Controller
+    {
+        // GET: Board
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
